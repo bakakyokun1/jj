f0cd651def9312b9be73a4dd7976ddc277fde96c
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-18 20:26:21 -0500

    Create imp.sh

---
+++ imp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Imperial March melody using beep
+beep -f 440 -l 500; sleep 0.05 # A
+beep -f 440 -l 500; sleep 0.05 # A
+beep -f 440 -l 500; sleep 0.05 # A
+beep -f 349 -l 350; sleep 0.05 # F
+beep -f 523 -l 150; sleep 0.05 # C
+beep -f 440 -l 500; sleep 0.05 # A
+beep -f 349 -l 350; sleep 0.05 # F
+beep -f 523 -l 150; sleep 0.05 # C
+beep -f 440 -l 1000; sleep 0.05 # A
+beep -f 659 -l 500; sleep 0.05 # E
+beep -f 659 -l 500; sleep 0.05 # E
+beep -f 659 -l 500; sleep 0.05 # E
+beep -f 698 -l 350; sleep 0.05 # F
+beep -f 523 -l 150; sleep 0.05 # C
+beep -f 415 -l 500; sleep 0.05 # G
+beep -f 349 -l 350; sleep 0.05 # F
+beep -f 523 -l 150; sleep 0.05 # C
+beep -f 440 -l 1000; sleep 0.05 # A
