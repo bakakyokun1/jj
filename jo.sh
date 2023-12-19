0650f8b54b8a68677ebe5f21a4be60265743c96b
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-18 20:07:09 -0500

    Create jo.sh

---
+++ jo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Jingle Bells melody using beep
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 600; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 600; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 523 -l 300; sleep 0.1  # C
+beep -f 587 -l 300; sleep 0.1  # D
+beep -f 523 -l 900; sleep 0.3  # C
+
+beep -f 784 -l 300; sleep 0.1  # G
+beep -f 784 -l 300; sleep 0.1  # G
+beep -f 784 -l 600; sleep 0.1  # G
+beep -f 784 -l 300; sleep 0.1  # G
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+beep -f 659 -l 300; sleep 0.1  # E
+
+sleep 0.1
+beep -f 523 -l 300; sleep 0.1  # C
+beep -f 523 -l 300; sleep 0.1  # C
+beep -f 523 -l 300; sleep 0.1  # C
+beep -f 523 -l 300; sleep 0.1  # C
+beep -f 587 -l 300; sleep 0.1  # D
+beep -f 523 -l 900; sleep 0.3  # C
