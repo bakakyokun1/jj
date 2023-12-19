2ccd8b202f3768f65bf89de94f05a8902bdc53a2
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-18 20:22:57 -0500

    Create jof.sh

---
+++ jof.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Jingle Bells melody using beep
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 293 -l 100; sleep 0.05 # D
+beep -f 329 -l 100; sleep 0.05 # E
+beep -f 293 -l 200; sleep 0.2  # D
+beep -f 349 -l 100; sleep 0.05 # F
+beep -f 392 -l 200; sleep 0.4  # G
+
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.05 # G
+beep -f 392 -l 100; sleep 0.2  # G
+beep -f 293 -l 100; sleep 0.05 # D
+beep -f 329 -l 100; sleep 0.05 # E
+beep -f 293 -l 200; sleep 0.2  # D
+beep -f 349 -l 100; sleep 0.05 # F
+beep -f 392 -l 200; sleep 0.4  # G
