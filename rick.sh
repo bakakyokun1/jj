22ff21a23fb03b6c79167dcd852555864d9a9c38
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-26 13:07:24 -0500

    Create rick.sh

---
+++ rick.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Simplified "Never Gonna Give You Up" melody using beep
+beep -f 392 -l 200 # G4
+sleep 0.1
+beep -f 392 -l 200 # G4
+sleep 0.1
+beep -f 392 -l 200 # G4
+sleep 0.1
+beep -f 349 -l 600 # F4
+sleep 0.1
+beep -f 294 -l 600 # D4
+sleep 0.1
+
+beep -f 392 -l 200 # G4
+sleep 0.1
+beep -f 349 -l 600 # F4
+sleep 0.1
+beep -f 294 -l 600 # D4
+sleep 0.1
+
+beep -f 392 -l 200 # G4
+sleep 0.1
+beep -f 294 -l 200 # D4
+sleep 0.1
+beep -f 261 -l 200 # C4
+sleep 0.1
+beep -f 294 -l 800 # D4
+sleep 0.4
