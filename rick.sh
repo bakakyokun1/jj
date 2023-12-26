e46d38b2868eb9efe91661d0dcf6d185838fa760
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-26 13:10:04 -0500

    rick.sh

--- rick.sh
+++ rick.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# Simplified "Never Gonna Give You Up" melody using beep
-beep -f 392 -l 200 # G4
-sleep 0.1
-beep -f 392 -l 200 # G4
-sleep 0.1
-beep -f 392 -l 200 # G4
-sleep 0.1
-beep -f 349 -l 600 # F4
-sleep 0.1
-beep -f 294 -l 600 # D4
-sleep 0.1
+# "Never Gonna Give You Up" by Rick Astley, simplified for beep
+# Adjust the tempo and notes as needed
 
-beep -f 392 -l 200 # G4
+# Verse
+beep -f 330 -l 400 # E4
 sleep 0.1
-beep -f 349 -l 600 # F4
+beep -f 330 -l 400 # E4
 sleep 0.1
-beep -f 294 -l 600 # D4
+beep -f 330 -l 400 # E4
 sleep 0.1
-
-beep -f 392 -l 200 # G4
+beep -f 330 -l 400 # E4
 sleep 0.1
-beep -f 294 -l 200 # D4
+beep -f 370 -l 400 # F#4
 sleep 0.1
-beep -f 261 -l 200 # C4
+beep -f 392 -l 400 # G4
 sleep 0.1
-beep -f 294 -l 800 # D4
+beep -f 370 -l 400 # F#4
+sleep 0.1
+beep -f 330 -l 800 # E4
 sleep 0.4
+
+# "Never gonna give you up"
+beep -f 330 -l 400 # E4
+sleep 0.1
+beep -f 370 -l 200 # F#4
+sleep 0.1
+beep -f 392 -l 400 # G4
+sleep 0.1
+beep -f 330 -l 400 # E4
+sleep 0.1
+
+# "Never gonna let you down"
+beep -f 330 -l 400 # E4
+sleep 0.1
+beep -f 370 -l 200 # F#4
+sleep 0.1
+beep -f 392 -l 400 # G4
+sleep 0.1
+beep -f 330 -l 400 # E4
+sleep 0.1
