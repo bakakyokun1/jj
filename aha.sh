41ecb1b9a57279a32ce6e8a1fafc176f14361d41
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2023-12-26 13:12:16 -0500

    aha.sh

---
+++ aha.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# "Take On Me" opening riff and part of the chorus, simplified for beep
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 494 -l 400 # B4
+sleep 0.1
+beep -f 523 -l 400 # C5
+sleep 0.1
+
+beep -f 587 -l 200 # D5
+sleep 0.05
+beep -f 587 -l 200 # D5
+sleep 0.05
+beep -f 587 -l 200 # D5
+sleep 0.05
+beep -f 440 -l 400 # A4
+sleep 0.1
+beep -f 523 -l 400 # C5
+sleep 0.1
+
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 659 -l 200 # E5
+sleep 0.05
+beep -f 494 -l 400 # B4
+sleep 0.1
+beep -f 523 -l 400 # C5
+sleep 0.1
+
+# Chorus part "Take on me"
+beep -f 784 -l 400 # G5
+sleep 0.1
+beep -f 880 -l 400 # A5
+sleep 0.1
+beep -f 988 -l 800 # B5
+sleep 0.4
+
+# ...continue with additional notes as desired
+
