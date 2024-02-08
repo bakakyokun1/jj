3ec78ef17fd004ef17488c1a0b6b39e1280f32d9
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2024-02-07 19:00:13 -0500

    twi.sh

---
+++ twi.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Twinkle, Twinkle, Little Star
+
+# Play the melody
+beep -f 261.63 -l 500; sleep 0.05 # C
+beep -f 261.63 -l 500; sleep 0.05 # C
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 440.00 -l 500; sleep 0.05 # A
+beep -f 440.00 -l 500; sleep 0.05 # A
+beep -f 392.00 -l 1000; sleep 0.05 # G
+
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 293.66 -l 500; sleep 0.05 # D
+beep -f 293.66 -l 500; sleep 0.05 # D
+beep -f 261.63 -l 1000; sleep 0.05 # C
+
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 293.66 -l 1000; sleep 0.05 # D
+
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 293.66 -l 1000; sleep 0.05 # D
+
+beep -f 261.63 -l 500; sleep 0.05 # C
+beep -f 261.63 -l 500; sleep 0.05 # C
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 392.00 -l 500; sleep 0.05 # G
+beep -f 440.00 -l 500; sleep 0.05 # A
+beep -f 440.00 -l 500; sleep 0.05 # A
+beep -f 392.00 -l 1000; sleep 0.05 # G
+
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 349.23 -l 500; sleep 0.05 # F
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 329.63 -l 500; sleep 0.05 # E
+beep -f 293.66 -l 500; sleep 0.05 # D
+beep -f 293.66 -l 500; sleep 0.05 # D
+beep -f 261.63 -l 1000; sleep 0.05 # C
