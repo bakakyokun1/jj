dd0e69cae1bfbaf82540f5e8117f26a2c4ef469a
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2024-07-09 15:26:58 -0400

    Update mario.sh

--- mario.sh
+++ mario.sh
@@ -54,3 +54,46 @@ sleep 0.1
 beep -f 587 -l 150  # D5
 sleep 0.1
 beep -f 494 -l 150  # B4
+
+# Repeat the main motif to complete the theme
+sleep 0.1
+
+beep -f 523 -l 200  # C5
+sleep 0.1
+beep -f 392 -l 200  # G4
+sleep 0.1
+beep -f 330 -l 200  # E4
+
+sleep 0.1
+
+beep -f 440 -l 100  # A4
+sleep 0.05
+beep -f 494 -l 100  # B4
+sleep 0.05
+beep -f 466 -l 100  # A#4
+sleep 0.05
+beep -f 440 -l 100  # A4
+
+sleep 0.05
+
+beep -f 392 -l 150  # G4
+sleep 0.1
+beep -f 659 -l 150  # E5
+sleep 0.1
+beep -f 784 -l 150  # G5
+sleep 0.1
+beep -f 880 -l 150  # A5
+
+sleep 0.1
+
+beep -f 698 -l 150  # F5
+sleep 0.1
+beep -f 784 -l 150  # G5
+sleep 0.1
+beep -f 659 -l 150  # E5
+sleep 0.1
+beep -f 523 -l 150  # C5
+sleep 0.1
+beep -f 587 -l 150  # D5
+sleep 0.1
+beep -f 494 -l 150  # B4
