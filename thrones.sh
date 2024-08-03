f690869b0cce7a758f35ed46728186bcf4abf779
bakakyokun1 <154287209+bakakyokun1@users.noreply.github.com>
2024-08-02 20:18:52 -0400

    Create thrones.sh

---
+++ thrones.sh
@@ -0,0 +1,83 @@
+# Game of Thrones Theme
+
+beep -f 440 -l 500   # A4
+sleep 0.1
+beep -f 523 -l 500   # C5
+sleep 0.1
+beep -f 587 -l 500   # D5
+sleep 0.1
+beep -f 659 -l 500   # E5
+sleep 0.1
+beep -f 698 -l 500   # F5
+sleep 0.1
+beep -f 784 -l 500   # G5
+
+sleep 0.2
+
+beep -f 440 -l 500   # A4
+sleep 0.1
+beep -f 523 -l 500   # C5
+sleep 0.1
+beep -f 587 -l 500   # D5
+sleep 0.1
+beep -f 659 -l 500   # E5
+sleep 0.1
+beep -f 698 -l 500   # F5
+sleep 0.1
+beep -f 784 -l 500   # G5
+
+sleep 0.2
+
+beep -f 440 -l 500   # A4
+sleep 0.1
+beep -f 523 -l 500   # C5
+sleep 0.1
+beep -f 587 -l 500   # D5
+sleep 0.1
+beep -f 659 -l 500   # E5
+sleep 0.1
+beep -f 698 -l 500   # F5
+sleep 0.1
+beep -f 784 -l 500   # G5
+
+sleep 0.2
+
+beep -f 880 -l 700   # A5
+sleep 0.2
+beep -f 784 -l 700   # G5
+sleep 0.2
+beep -f 698 -l 700   # F5
+sleep 0.2
+beep -f 659 -l 700   # E5
+sleep 0.2
+beep -f 587 -l 700   # D5
+sleep 0.2
+beep -f 523 -l 700   # C5
+
+sleep 0.3
+
+beep -f 880 -l 700   # A5
+sleep 0.2
+beep -f 784 -l 700   # G5
+sleep 0.2
+beep -f 698 -l 700   # F5
+sleep 0.2
+beep -f 659 -l 700   # E5
+sleep 0.2
+beep -f 587 -l 700   # D5
+sleep 0.2
+beep -f 523 -l 700   # C5
+
+sleep 0.3
+
+beep -f 880 -l 700   # A5
+sleep 0.2
+beep -f 784 -l 700   # G5
+sleep 0.2
+beep -f 698 -l 700   # F5
+sleep 0.2
+beep -f 659 -l 700   # E5
+sleep 0.2
+beep -f 587 -l 700   # D5
+sleep 0.2
+beep -f 523 -l 700   # C5
